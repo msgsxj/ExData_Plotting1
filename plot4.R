38811729b7ba4d238773c965cce4dc4a24fe6f2b
msgsxj <506927569@qq.com>
2018-04-22 01:59:27 +0800

    'firstpush'

---
+++ plot4.R
@@ -0,0 +1,72 @@
+getdata <- function(){
+    # set path
+    setwd("C:/Users/msgsxj/Desktop/coursera/Exploratory Data Analysis/ExData_Plotting1")
+    
+    # get data
+    ## create data folder
+    if (!file.exists("data")){
+        dir.create("data")
+    }
+    ## cd data
+    setwd("./data/")
+    ## download data from url
+    if (!file.exists("household_power_consumption.zip")){
+        url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip'
+        download.file(url, destfile = "./household_power_consumption.zip", method = "curl")
+    }
+    ## unzip data
+    if (!file.exists("household_power_consumption.txt")){
+        unzip('household_power_consumption.zip')
+    }
+    ## if not exist the selected data.csv, create it. 3 steps as followed:
+    ## 1. read the original .txt file line by line, it will occupy the least memory
+    ## 2. create data_selected.csv, write the feature name line of .txt into .csv
+    ## 3. for each line in .txt, if its head match '^[12]/2/2007', concatenate '\n' 
+    ##    with this line without ' ', write into .csv
+    if (!file.exists("data_selected.csv")){
+        f <- file("household_power_consumption.txt", "r")
+        f_selected <- file("data_selected.csv", "w")
+        feature_name <- readLines(f, n = 1)
+        cat(feature_name, file = f_selected)
+        oneline <- readLines(f, n = 1)
+        while(length(oneline) != 0){
+            if(length(grep('^[12]/2/2007', oneline, value = T)) != 0){
+                value <- paste('\n', oneline, sep="")
+                cat(value, file = f_selected)
+            }
+            oneline <- readLines(f, n = 1)
+        }
+        close(f_selected)
+        close(f)
+    }
+    ## read selected data
+    data <- read.table("data_selected.csv", sep = ";", header = TRUE, stringsAsFactors = F)
+    ## convert the Time into time in R 
+    data$Time <- paste(data$Date, data$Time)
+    data <- subset(data, select = -Date )
+    data$Time <- strptime(data$Time, "%d/%m/%Y %H:%M:%S")
+    
+    # get back to the root path
+    setwd("C:/Users/msgsxj/Desktop/coursera/Exploratory Data Analysis/ExData_Plotting1")
+    return(data)
+}
+
+plot4 <- function(){
+    data <- getdata()
+    png("plot4.png", bg = "transparent", width = 480, height = 480)
+    par(mfcol = c(2, 2))
+    with(airquality, {
+        # 1
+        with(data, plot(Time, Global_active_power, type = 'l', ylab = 'Global Active Power', xlab = ''))
+        # 2
+        with(data, plot(Time, Sub_metering_1, type = 'l', col = 'black', xlab = '', ylab = 'Energy sub metering'))
+        with(data, lines(Time, Sub_metering_2, type = 'l', col = 'red'))
+        with(data, lines(Time, Sub_metering_3, type = 'l', col = 'blue'))
+        legend("topright", lty = 1, col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), bty = "n")
+        # 3
+        with(data, plot(Time, Voltage, type = 'l', ylab = 'Voltage', xlab = 'datetime'))
+        # 4
+        with(data, plot(Time, Global_reactive_power, type = 'l', ylab = 'Global_reactive_power', xlab = 'datetime'))
+    })
+    dev.off()
+}
